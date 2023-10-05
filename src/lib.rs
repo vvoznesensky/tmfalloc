5a1c24daf63e523a5c65da42047e0ac2c963d830
Vladimir Voznesenskiy <vvoznesensky@gmail.com>
2023-10-05 20:43:21 +0300

    Moved Allocator to submodule allocator

--- src/lib.rs
+++ src/lib.rs
@@ -227,17 +227,16 @@ use libc;
 use libc::{c_int, c_void};
 use nix::sys::signal::{
     sigaction, SigAction, SigHandler, SaFlags, SigSet, Signal};
-use std::alloc;
 use std::cell::RefCell;
 use std::collections::BTreeMap;
-use intrusive_collections::Bound::Included as IntrusiveIncluded;
 use intrusive_collections::intrusive_adapter;
 use intrusive_collections::{RBTreeLink, RBTree, KeyAdapter, UnsafeRef};
 use std::marker;
 use std::mem::ManuallyDrop;
 use std::ops;
-use std::ptr;
 use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard, Mutex, Arc};
+pub mod allocator;
+pub use allocator::Allocator;
 
 // Panic on file problems.
 macro_rules! panic_syserr {
@@ -404,7 +403,8 @@ impl<'a, Root: 'a> Holder<'a, Root> {
         prep_header(&s, magick, shown, arena_size, new_root)?;
         Ok(s)
     }
-    /// Shared-lock the storage and get [Reader] smart pointer to Root instance
+    /// Shared-lock the storage and get [Reader] smart pointer to the Root
+    /// instance inside
     pub fn read(&self) -> Reader::<Root> {
         let guard = self.arena.read().unwrap();
         {
@@ -430,8 +430,8 @@ impl<'a, Root: 'a> Holder<'a, Root> {
         rv.setseg();
         rv
     }
-    /// Exclusive-lock the storage and get [Writer] smart pointer to Root
-    /// instance
+    /// Exclusive-lock the storage and get [Writer] smart pointer to the Root
+    /// instance inside
     pub fn write(&mut self) -> Writer<Root> { self.internal_write::<true>() }
 
     /// Returns the numeric address of the arena space beginning
@@ -525,73 +525,6 @@ fn flock_r(fd: c_int) {
 }
 fn unflock(fd: c_int) {
     panic_syserr!(libc::flock(fd, libc::LOCK_UN));
-}
-
-// The address and size-address orders (de)allocation.
-fn allocate(from_size: (*const u8, usize), layout: alloc::Layout) ->
-        std::result::Result<ptr::NonNull<[u8]>, alloc::AllocError> {
-    let s = layout.size() + layout.padding_needed_for(ALLOCATION_QUANTUM);
-    let h = unsafe{(from_size.0 as *mut HeaderOfHeader).as_mut()}.unwrap();
-    let c = h.by_size_address.lower_bound(
-                    IntrusiveIncluded(&(s, std::ptr::null::<FreeBlock>())));
-    match c.get() {
-        None => Err(alloc::AllocError),
-        Some(r) => {
-            let p = r as *const FreeBlock;
-            let rs = r.size;
-            FreeBlock::finalize(h, p);
-            if rs > s {
-                let n = unsafe{ p.byte_add(s) } as *mut FreeBlock;
-                FreeBlock::initialize(h, n, rs - s);
-            }
-            print!("allocated address is {:X}, size {}\n", p as usize, s);
-            Ok(ptr::NonNull::slice_from_raw_parts(
-                                ptr::NonNull::new(p as *mut u8).unwrap(), s))
-        }
-    }
-}
-
-unsafe fn deallocate(from_size: (*const u8, usize), ptr: ptr::NonNull<u8>,
-                     layout: alloc::Layout) {
-    let s = layout.size() + layout.padding_needed_for(ALLOCATION_QUANTUM);
-    print!("deallocating address is {:X}, size {}\n", ptr.as_ptr() as usize, s);
-    assert!(s % ALLOCATION_QUANTUM == 0);
-    let h = unsafe{(from_size.0 as *mut HeaderOfHeader).as_mut()}.unwrap();
-    let p = ptr.as_ptr() as *const FreeBlock;
-    // XXX These cursors point to neighbours, so could be optimized?
-    let cu = h.by_address.lower_bound(IntrusiveIncluded(&p));
-    let cl = h.by_address.upper_bound(IntrusiveIncluded(&p));
-    let ln = match cl.get() { // lower neighbour
-        None => None,
-        Some(l) => if (l as *const FreeBlock).byte_add(l.size) < p { None }
-                    else { Some(l as *const FreeBlock) }
-    };
-    let un = match cu.get() { // upper neighbour
-        None => None,
-        Some(u) => if p.byte_add(s) < u as *const FreeBlock { None }
-                    else { Some((u as *const FreeBlock, u.size)) }
-    };
-    match ln {
-        None => match un {
-            None => FreeBlock::initialize(h, p, s),
-            Some((up, us)) => {
-                FreeBlock::finalize(h, up as *const FreeBlock);
-                FreeBlock::initialize(h, p, s + us);
-            }
-        },
-        Some(lp) => {
-            let lr = lp.cast_mut().as_mut().unwrap();
-            h.by_size_address.cursor_mut_from_ptr(lp).remove();
-            match un {
-                None => lr.size += s,
-                Some((up, us)) => {
-                    FreeBlock::finalize(h, up);
-                    lr.size += s + us;
-                }
-            };
-            h.by_size_address.insert(unsafe { UnsafeRef::from_raw(lp) } );
-        }
-    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -892,14 +825,18 @@ pub struct InternalWriter<'a, Root, const PAGES_WRITABLE: bool> {
 }
 impl<'a, Root, const PAGES_WRITABLE: bool>
         InternalWriter<'a, Root, PAGES_WRITABLE> {
+    /// Rollback the current transaction. Automatically called in
+    /// [InternalWriter::drop] method.
     pub fn rollback(&self) {
         let g = &self.guard;
         rollback::<PAGES_WRITABLE>(*g.fd, *g.log_fd, g.mem.0, g.mem.1);
     }
+    /// Commit the current transaction. Call to save the data.
     pub fn commit(&self) {
         let g = &self.guard;
         commit(*g.fd, *g.log_fd, g.mem.0, g.mem.1);
     }
+    /// Create allocator to use in collections and containers.
     pub fn allocator(&self) -> Allocator {
         Allocator { address: self.guard.mem.0 as usize }
     }
@@ -982,38 +919,6 @@ impl<'a> KeyAdapter<'a> for BySizeAddressAdapter {
     type Key = (usize, *const FreeBlock);
     fn get_key(&self, x: &'a FreeBlock) -> Self::Key {
         (x.size, x as *const FreeBlock)
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-/// Allocator applicable for standard containers to make them persistent
-///
-/// Create [Writer] by [Holder::write] in the same thread before allocation,
-/// deallocation and other persistent storage update.
-#[derive(Clone)]
-pub struct Allocator {
-    address: usize,
-}
-
-impl Allocator {
-    pub fn address(&self) -> usize { self.address }
-    // Find the applicable memory segment for the given allocator
-    fn segment(&self) -> (*const u8, usize) {
-        let a = self.address as *const c_void;
-        MEM_MAP.with_borrow(|mb| {
-            let ta = mb.get(&a).unwrap();
-            (a as *const u8, ta.size)
-        })
-    }
-}
-
-unsafe impl alloc::Allocator for Allocator {
-    fn allocate(&self, layout: alloc::Layout) ->
-            std::result::Result<ptr::NonNull<[u8]>, alloc::AllocError> {
-        allocate(self.segment(), layout)
-    }
-    unsafe fn deallocate(&self, p: ptr::NonNull<u8>, layout: alloc::Layout) {
-        deallocate(self.segment(), p, layout)
     }
 }
 
